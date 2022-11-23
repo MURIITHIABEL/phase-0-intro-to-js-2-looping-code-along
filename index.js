9883fedd65c1c756a014468e3779fc6074d395ae
Abel Muriithi <abelmuriithi04@gmail.com>
2022-11-24 01:55:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, event) {
+    return  names.map(name=> `Thank you, ${name}, for the wonderful ${event} gift!`)
+}
+
+console.log(writeCards(names, "suprise"))
+
+
+
+
+var i= "";
+
+function countDown(i)
+
+{
+
+    while( i >=0)
+
+    {
+        console.log(i);
+        i--;
+    }
+}
+countDown();
